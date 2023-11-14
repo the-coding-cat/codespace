d65800b9b0ed5909b15c9f1b81be9b18f9f906f5
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-11-14 16:59:17 +0000

    Tue, Nov 14, 2023, 4:59 PM +00:00

--- hello.c
+++ hello.c
@@ -3,19 +3,6 @@
 
 int main(void)
 {
-    int x = get_int("What's x?")
-    int y = get_int("Whats y?")
-
-    if (x < y)
-    {
-        printf("x is less than y\n")
-    }
-    if else (x > y)
-    {
-        printf("x is greater than y\n")
-    }
-    else
-    {
-        printf("x and y are equal")
-    }
+    string x = get_string("What's x?");
+    printf("haha, nice\n");
 }
