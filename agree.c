f4b2a1c0743a5ea085a90e556a926562dfd0f8a8
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-11-15 15:58:46 +0000

    Wed, Nov 15, 2023, 3:58 PM +00:00

--- agree.c
+++ agree.c
@@ -5,11 +5,11 @@ int main(void)
 {
     char c = get_char("Do you agree? ");
 
-    if (c == 'y')
+    if (c == 'y' || c == 'Y')
     {
         printf("Agreed\n");
     }
-    if (c == 'n')
+    if (c == 'n' || c == 'N')
     {
         printf("Not agreed.\n");
     }
