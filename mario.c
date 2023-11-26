d587add1689a8f2d87f6da9101a556c2783f2f44
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-11-26 01:02:40 +0000

    Sun, Nov 26, 2023, 1:02 AM +00:00

--- mario.c
+++ mario.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
+    int n = get_int("Pyramid of blocks hieght: ");
     while (n < 1)
     {
-        int n = get_int("Pyramid of blocks hieght: ")
+        int n = get_int("Pyramid of blocks hieght: ");
     }
     for (i = 0; i < n; i++)
-    { 
+    {
         printf("#");
     }
     printf(" \n");
