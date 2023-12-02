f224f817ae38a12e017140e070e17420fff20901
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-12-02 23:52:06 +0000

    Sat, Dec 2, 2023, 11:52 PM +00:00

--- mario.c
+++ mario.c
@@ -3,13 +3,5 @@
 
 int main(void)
 {
-    while (n < 1)
-    {
-        int n = get_int("Pyramid of blocks hieght: ");
-    }
-    for (i = 0; i < n; i++)
-    {
-        printf("#");
-    }
-    printf(" \n");
+    
 }
