f78b63e8e8892b6bfa0bd1a11321bbec73ae8858
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-12-14 00:29:30 +0000

    Thu, Dec 14, 2023, 12:29 AM +00:00

--- projects/code.c
+++ projects/code.c
@@ -15,7 +15,14 @@ int main(void)
    \n = newline
    \t = tab
    */
-    printf("I like pizza!\n");
-    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    //printf("I like pizza!\n");
+    //printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    //printf("\"Four Scores and Seven years ago\" - Abe Lincoln");
+    /*if you need to print literal quotes put an escape sequence
+    before each of the backslashes.
+
+    If you need to print out "/" just put two // and it will print out one */
+
+    
     return 0;
 }
