457a9d49b1b6282c9eeb3a83394f3bec5675aab8
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-12-14 00:19:11 +0000

    Thu, Dec 14, 2023, 12:19 AM +00:00

---
+++ projects/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //one line comment
+    /*
+    Writitng essays here
+    google doc? Word? nah, I prefer IDE's
+    Why not its like my life isn't hard enough
+    Cringe
+    */
+   /* Escape sequence  = characters combination consisting of a backslash \
+   followed by a letter or combination of digits.
+   They follow specifu actions within a line or string of text.
+   \n = newline
+   \t = tab
+   */
+    printf("I like pizza!\n");
+    return 0;
+}
