f39dc1f7dcb6b4c41ae9e0fdc8d6236f92a7e168
Void Cat <148478659+the-coding-cat@users.noreply.github.com>
2023-12-20 15:10:22 +0000

    Wed, Dec 20, 2023, 3:10 PM +00:00

--- projects/code.c
+++ projects/code.c
@@ -10,6 +10,8 @@ int main(void)
     Why not its like my life isn't hard enough
     Cringe
     */
+
+    
    /* Escape sequence  = characters combination consisting of a backslash \
    followed by a letter or combination of digits.
    They follow specifu actions within a line or string of text.
